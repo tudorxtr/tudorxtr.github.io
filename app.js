e867b784154693ac945d34fb1ee7d6c49bda9cf4
Dutcas Tudor <dutcastudor@gmail.com>
2018-07-15 01:27:34 +0300

    js update

--- app.js
+++ app.js
@@ -142,31 +142,3 @@ function filterTasks(e){
         console.log(item);
     });
 }
-
-// Add to Home Screen
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-    btnAdd.style.display = 'block';
-});
-
-btnAdd.addEventListener('click', (e) => {
-    btnAdd.style.display = 'none';
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice
-    .then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-    } else {
-        console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-    });
-});
-  
-window.addEventListener('appinstalled', (evt) => {
-    app.logEvent('a2hs', 'installed');
-});
-
